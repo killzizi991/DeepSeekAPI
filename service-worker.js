22e817d709996aa448e0e4cbfc6ffc22627041ae
killzizi991 <killzizi@yandex.ru>
2025-09-16 09:34:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,34 @@
+const CACHE_NAME = 'deepseek-coder-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/css/themes.css',
+    '/js/app.js',
+    '/js/fileHandler.js',
+    '/js/githubApi.js',
+    '/js/ui.js',
+    '/js/storage.js',
+    '/lib/prism.js',
+    '/lib/prism.css'
+];
 
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
